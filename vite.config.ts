a554acb6a26e8e4d454a24d1e7d7a6aba9aead5e
Rahul <rahulmaharjan252@gmail.com>
2025-07-27 21:12:10 +0545

    deployment push

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "Rahul-Maharjan-Portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
